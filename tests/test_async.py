203a76175e3c525bc5e3244f3c7be4f9f82abf2b
Lily Foote <code@lilyf.org>
2024-04-24 11:31:29 +0100

    Simplify segfault reproduction
    This reliably segfaults with `use_rust` set to either `True` or `False`.
    I'm choosing `False` here because that should rule out Kolo's Rust
    extension as a contributing factor.

--- tests/test_async.py
+++ tests/test_async.py
@@ -1,7 +1,6 @@
 import asyncio
 
 import kolo
-import pytest
 
 
 async def async_add(*args: int) -> int:
@@ -17,8 +16,7 @@ async def multi_add():
     return await asyncio.gather(slow_add(1, 2, time=0.2), slow_add(3, 4, time=0.1))
 
 
-@pytest.mark.parametrize("use_rust", (False, True))
-def test_profile_tasks(use_rust):
-    with kolo.enabled({"use_rust": use_rust}):
+def test_profile_tasks():
+    with kolo.enabled({"use_rust": False}):
         results = asyncio.run(multi_add())
     assert results == [3, 7]
